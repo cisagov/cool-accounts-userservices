bb5affe83b98adfb2d029996604205cae4e547c1
Dave Redmin <david.redmin@trio.dhs.gov>
2021-01-26 16:21:23 -0500

    Define output

--- outputs.tf
+++ outputs.tf
@@ -1,24 +1,4 @@
-output "arn" {
-  value       = aws_instance.example.arn
-  description = "The EC2 instance ARN"
-}
-
-output "availability_zone" {
-  value       = aws_instance.example.availability_zone
-  description = "The AZ where the EC2 instance is deployed"
-}
-
-output "id" {
-  value       = aws_instance.example.id
-  description = "The EC2 instance ID"
-}
-
-output "private_ip" {
-  value       = aws_instance.example.private_ip
-  description = "The private IP of the EC2 instance"
-}
-
-output "subnet_id" {
-  value       = aws_instance.example.subnet_id
-  description = "The ID of the subnet where the EC2 instance is deployed"
+output "provisionaccount_role" {
+  value       = module.provisionaccount.provisionaccount_role
+  description = "The IAM role that allows sufficient permissions to provision all AWS resources in the User Services account."
 }
