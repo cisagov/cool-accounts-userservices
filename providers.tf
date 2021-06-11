ca3a200e263079726993f8566bd30389d903ca67
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2021-06-11 17:21:46 -0400

    Add default_tags to providers

--- providers.tf
+++ providers.tf
@@ -1,11 +1,14 @@
 # This is the "default" provider that is used to create resources
 # inside the COOL User Services account
 provider "aws" {
-  region = var.aws_region
+  default_tags {
+    tags = var.tags
+  }
   # Use this profile once the account has been bootstrapped.
   profile = "cool-userservices-provisionaccount"
   # Use this profile, defined using programmatic credentials for
   # AWSAdministratorAccess as obtained for the COOL User Services account
   # from the AWS SSO page, to bootstrap the account.
   # profile = "cool-userservices-account-admin"
+  region = var.aws_region
 }
