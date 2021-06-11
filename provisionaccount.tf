b86360fb66b04a8ed27742f5a9a61978f8001795
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2021-06-11 17:22:13 -0400

    Pass default tags via providers instead of directly to modules

--- provisionaccount.tf
+++ provisionaccount.tf
@@ -3,6 +3,5 @@ module "provisionaccount" {
 
   provisionaccount_role_description = var.provisionaccount_role_description
   provisionaccount_role_name        = var.provisionaccount_role_name
-  tags                              = var.tags
   users_account_id                  = var.users_account_id
 }
