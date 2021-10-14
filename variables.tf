a5478dc167fd413cb5e8cae3c561d14d6e5ea55e
David Redmin <david.redmin@trio.dhs.gov>
2021-10-14 17:25:01 -0400

    Add SSM-related variables

--- variables.tf
+++ variables.tf
@@ -33,6 +33,30 @@ variable "provisionaccount_role_name" {
   default     = "ProvisionAccount"
 }
 
+variable "provisionssmdocument_policy_description" {
+  type        = string
+  description = "The description to associate with the IAM policy that allows sufficient permissions to provision the SSM Document resource in the User Services account."
+  default     = "Allows sufficient permissions to provision the SSM Document resource in the User Services account."
+}
+
+variable "provisionssmdocument_policy_name" {
+  type        = string
+  description = "The name to assign the IAM policy that allows sufficient permissions to provision the SSM Document resource in the User Services account."
+  default     = "ProvisionSSMDocument"
+}
+
+variable "ssmsession_role_description" {
+  type        = string
+  description = "The description to associate with the IAM role (and policy) that allows creation of SSM SessionManager sessions to any EC2 instance in this account."
+  default     = "Allows creation of SSM SessionManager sessions to any EC2 instance in this account."
+}
+
+variable "ssmsession_role_name" {
+  type        = string
+  description = "The name to assign the IAM role (and policy) that allows creation of SSM SessionManager sessions to any EC2 instance in this account."
+  default     = "StartStopSSMSession"
+}
+
 variable "tags" {
   type        = map(string)
   description = "Tags to apply to all AWS resources provisioned."
