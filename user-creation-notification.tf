6134e17e39738fd7b3454e07747b7b2241c36908
David Redmin <david.redmin@gwe.cisa.dhs.gov>
2023-12-29 10:20:55 -0500

    Update code and comments to reflect repo name change and additional functionality
    See cisagov/user-group-mod-alert-tf-module#26 for more info.

--- user-creation-notification.tf
+++ user-creation-notification.tf
@@ -1,6 +1,7 @@
 # ------------------------------------------------------------------------------
-# Create the SNS topic that allows email to be sent when a new IAM or
-# SSO user is created.  Subscribe the account email to the new SNS
+# Create the SNS topic that allows email to be sent whenever a new IAM or SSO
+# user is created or deleted, a user is added or removed from a group, or a
+# group is created or deleted.  Subscribe the account email to the new SNS
 # topic.
 # ------------------------------------------------------------------------------
 
@@ -17,15 +18,15 @@ data "aws_iam_policy_document" "sns_topic_access_policy_doc" {
     }
 
     resources = [
-      # We can't use module.new_user_sns.sns_topic.arn here because it
+      # We can't use module.user_group_mod_sns.sns_topic.arn here because it
       # creates a cycle; fortunately, we can create the SNS topic ARN
       # manually.
-      "arn:aws:sns:${var.aws_region}:${local.userservices_account_id}:new-user-created",
+      "arn:aws:sns:${var.aws_region}:${local.userservices_account_id}:user-or-group-modified",
     ]
   }
 }
 
-module "new_user_sns" {
+module "user_group_mod_sns" {
   providers = {
     aws                         = aws
     aws.organizations_read_only = aws.organizationsreadonly
@@ -33,21 +34,22 @@ module "new_user_sns" {
   source = "github.com/cisagov/sns-send-to-account-email-tf-module"
 
   topic_access_policy = data.aws_iam_policy_document.sns_topic_access_policy_doc.json
-  topic_display_name  = "New IAM or SSO user created"
-  topic_name          = "new-user-created"
+  topic_display_name  = "IAM or SSO user or group modified"
+  topic_name          = "user-or-group-modified"
 }
 
 # ------------------------------------------------------------------------------
-# Create the EventBridge event rule that is triggered whenever a new
-# IAM or SSO user is created.  Connect this rule to the SNS topic
-# created above.
+# Create the EventBridge event rule that is triggered whenever a new IAM or SSO
+# user is created or deleted, a user is added or removed from a group, or a
+# group is created or deleted.  Connect this rule to the SNS topic created
+# above.
 # ------------------------------------------------------------------------------
 
-module "new_user_event" {
+module "user_group_mod_event" {
   providers = {
     aws = aws
   }
-  source = "github.com/cisagov/new-user-alert-tf-module"
+  source = "github.com/cisagov/user-group-mod-alert-tf-module"
 
-  target_arn = module.new_user_sns.sns_topic.arn
+  target_arn = module.user_group_mod_sns.sns_topic.arn
 }
