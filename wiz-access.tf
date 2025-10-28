3b74f421f0b779a2e16d17113a10f0ff2fc3e0aa
David Redmin <david.redmin@gwe.cisa.dhs.gov>
2025-10-28 16:18:28 -0400

    Remove extraneous blank line

--- wiz-access.tf
+++ wiz-access.tf
@@ -6,7 +6,6 @@
 module "wiz" {
   source = "https://wizio-public-fedramp.s3-us-gov-west-1.amazonaws.com/deployment-v3/aws/terraform/2209/wiz-aws-native-terraform-terraform-module.zip"
 
-
   cloud-cost-scanning       = false
   data-scanning             = true
   eks-scanning              = true
