f5f8ddd82cca45f619b9301820f7886e76a50bd8
David Redmin <david.redmin@gwe.cisa.dhs.gov>
2025-10-28 13:39:26 -0400

    Add variables needed for Wiz module

--- variables.tf
+++ variables.tf
@@ -16,6 +16,22 @@ variable "lambda_key" {
   type        = string
 }
 
+variable "wiz_external_id" {
+  description = "The external ID of the Wiz AWS Connector.  This value must be retrieved from the Wiz portal when creating the AWS Connector."
+  nullable    = false
+  type        = string
+  validation {
+    condition     = can(regex("^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$", var.wiz_external_id))
+    error_message = "The external_id must match the pattern XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX (UUID format)."
+  }
+}
+
+variable "wiz_remote_arn" {
+  description = "The AWS Trust Policy Role ARN for your Wiz data center.  It can be retrieved from the Wiz portal (User Settings, Tenant)."
+  nullable    = false
+  type        = string
+}
+
 # ------------------------------------------------------------------------------
 # OPTIONAL PARAMETERS
 #
